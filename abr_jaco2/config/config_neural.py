d2b545a85c390f55efeb75410bd9a4ef53063273
travis dewolf <travis.dewolf@gmail.com>
2017-03-10 13:39:53 -0500

    consolidated config_neurals

--- abr_jaco2/config/config_neural.py
+++ abr_jaco2/config/config_neural.py
@@ -1,5 +1,6 @@
-# to get new scales and means run the get_scales_averages script
-# in jaco2/tests
+""" Configuration file for the Jaco 2 that includes the
+scaling parameters for running with dynamics adaptation. """
+
 import numpy as np
 
 from . import config
@@ -13,17 +14,15 @@ class robot_config(config.robot_config):
         super(robot_config, self).__init__(**kwargs)
 
         self.means = {
-            'q': np.array(
-                [-0.14497, 2.58329, 3.45419, 4.49042, -0.75031, 2.76977]),
-            'dq': np.array(
-                [-0.01337, 0.00192, 0.00324, 0.02502, -0.02226, -0.01342])
+            'q': np.ones(self.num_joints) * np.pi,
+            'dq': np.array([-0.01337, 0.00192, 0.00324,
+                            0.02502, -0.02226, -0.01342])
             }
 
         self.scales = {
-            'q': np.array(
-                [2.30399, 1.17437, 1.99535, 5.95806, 6.75731, 1.1359]),
-            'dq': np.array(
-                [1.22826, 2.0, 1.42348, 2.58221, 2.50768, 1.27004])
+            'q' : np.ones(self.num_joints) * np.pi,
+            'dq': np.array([1.22826, 2.0, 1.42348,
+                            2.58221, 2.50768, 1.27004])
             }
 
     def scaledown(self, name, x):
