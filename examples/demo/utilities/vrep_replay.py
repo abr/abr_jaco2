b3084612546c3972e9180d4250c55a7eb711e905
travis dewolf <travis.dewolf@gmail.com>
2017-03-20 15:24:35 -0400

    made vrep_replay independent of redis

--- examples/demo/utilities/vrep_replay.py
+++ examples/demo/utilities/vrep_replay.py
@@ -1,28 +1,31 @@
 """
-send recorded data to vrep display to replay a test script
-Once vrep_display.py is running, running this script will
-send joint angle and target information to replay the run
-with it's data saved in 'file_location'
+send joint angles and target information to Vrep to replay the run
+with data saved in 'file_location'
 """
 import numpy as np
-import redis
 import time
 
-redis_server = redis.StrictRedis(host='localhost')
-file_location = 'parameter_testing/adaptive/baseline/trial0'
-loop_delay = 0.0045
+import abr_control
 
-q = np.load('../data/%s/q.npz' % file_location)['arr_0']
-filtered_target = np.load('../data/%s/filtered_target.npz' % file_location)['arr_0']
+file_location = '../data/parameter_testing/compliant/baseline/trial0'
+loop_delay = 0.0015
 
-for ii in range(0, len(q)):
-    redis_server.set('q', '%.3f %.3f %.3f %.3f %.3f %.3f' %
-                     (q[ii,0],q[ii,1],q[ii,2],
-                      q[ii,3],q[ii,4],q[ii,5]))
+q = np.load('%s/q.npz' % file_location)['arr_0']
+filtered_target = np.load('%s/filtered_target.npz' % file_location)['arr_0']
 
-    redis_server.set(
-        'norm_target_xyz_robot_coords', '%.3f %.3f %.3f'
-        % (filtered_target[ii,0],
-        filtered_target[ii,1],
-        filtered_target[ii,2]))
-    time.sleep(loop_delay)
+# create our config, interface, and connect to VREP
+rc = abr_control.arms.jaco2.config(
+    use_cython=True, hand_attached=True)
+interface = abr_control.interfaces.vrep(rc)
+
+try:
+    interface.connect()
+
+    for ii in range(0, len(q)):
+        interface.set_position(q[ii])
+        interface.set_xyz('target', filtered_target[ii])
+
+        time.sleep(loop_delay)
+
+finally:
+    interface.disconnect()
