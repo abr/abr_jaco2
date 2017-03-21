98cc8ae7c89ae72f2fa5910b5684d690de5e2bfc
Pawel Jaworski <p3jawors@gmail.com>
2017-03-21 14:52:18 -0400

    renamed interface_files interface, moved interface inside folder, other cleanup

--- examples/gen_zeros.py
+++
@@ -1,9 +0,0 @@
-# TO DO: MOVE TO CONTROL/UTILS
-
-# Generates zeros weight file for first run of learning
-
-import numpy as np
-
-def __init__(filename='default_name', dim_in=6, n_neurons=1000):
-    weights=np.zeros((dim_in,n_neurons))
-    np.savez_compressed('data/learning_osc/%s/%i_neurons/zeros' %(filename,n_neurons), weights=[weights])
