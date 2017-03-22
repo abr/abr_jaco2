bcc04c84e684e2aa35424a60da3e71765dc17eb1
travis dewolf <travis.dewolf@gmail.com>
2017-03-22 11:03:52 -0400

    renamed interface_files interface, moved interface inside folder, other cleanup

--- abr_jaco2/__init__.py
+++ abr_jaco2/__init__.py
@@ -1,6 +1,5 @@
-from .config.config import robot_config
-from .config.config_neural import robot_config as robot_config_neural
-from .interface import interface
-from .interface_files import jaco2_rs485
-from . import signals
-from .KBHit import KBHit
+from .config.config import robot_config as config
+from .config.config_neural import robot_config as config_neural
+from .interface.interface import interface
+from .interface import jaco2_rs485
+from . import utils
