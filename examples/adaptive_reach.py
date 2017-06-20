e3b9235afc7e3985a456a47afdaef65ca055ff6f
Pawel Jaworski <p3jawors@gmail.com>
2017-06-20 15:50:03 -0400

    Data collecting prep
    - can automatically save and load weights
    - can automatically save tracked data to same location as weights

--- examples/adaptive_reach.py
+++ examples/adaptive_reach.py
@@ -3,6 +3,7 @@ Move the jaco2 to a target position with an adaptive controller
 that will account for a 2lb weight in its hand
 """
 import numpy as np
+import timeit
 import traceback
 
 from abr_control.controllers import OSC, signals
@@ -35,13 +36,9 @@ interface = abr_jaco2.Interface(robot_config)
 
 TARGET_XYZ = np.array([.57, .03, .87])
 
-# TODO why does this not load?
-# weights_file = ('~/.cache/abr_control/saved_weights'
-#                 + '/does_this_work/trial0/run0.npz')
-weights_file = ['run0.npz']
-weights = np.load(weights_file[0])['weights']
-print('WEIGHTS FILE IS: ',weights)
-
+# weights_file = '~/.cache/abr_control/saved_weights/does_this_work/trial0/run9.npz'
+weights_file = None
+test_name = 'test1'
 # create our adaptive controller
 adapt = signals.DynamicsAdaptation(
     robot_config, backend='nengo_spinnaker',
@@ -50,7 +47,11 @@ adapt = signals.DynamicsAdaptation(
     weights_file=weights_file,
     pes_learning_rate=1e-3,
     intercepts=(-0.1, 1.0),
-    spiking=True)
+    spiking=True,
+    trial=None,
+    run=None,
+    test_name=test_name,
+    autoload=True)
 
 # connect to and initialize the arm
 interface.connect()
@@ -58,10 +59,10 @@ interface.init_position_mode()
 interface.send_target_angles(robot_config.INIT_TORQUE_POSITION)
 
 # set up lists for tracking data
-ee_track = []
-target_track = []
-
-
+time_track = []
+q_track = []
+u_track = []
+adapt_track = []
 try:
     interface.init_force_mode()
     # get the end-effector's initial position
@@ -69,6 +70,7 @@ try:
     count = 0
 
     while 1:
+        start = timeit.default_timer()
         # get joint angle and velocity feedback
         feedback = interface.get_feedback()
         q = feedback['q']
@@ -102,15 +104,17 @@ try:
 
         # calculate end-effector position
         ee_xyz = robot_config.Tx('EE', q=q)
+
+        end = timeit.default_timer() - start
         # track data
-        ee_track.append(np.copy(ee_xyz))
-        target_track.append(np.copy(TARGET_XYZ))
+        time_track.append(np.copy(end))
+        q_track.append(np.copy(q))
+        u_track.append(np.copy(u))
+        adapt_track.append(np.copy(u_adapt))
 
         if count % 100 == 0:
-            # print('adapt: ', u_adapt * time_scale)
             error = np.sqrt(np.sum((ee_xyz - TARGET_XYZ)**2))
             print('error: ', error)
-            print('adaptive: ', u_adapt * time_scale)
 
         count += 1
 
@@ -124,24 +128,35 @@ finally:
     interface.disconnect()
 
     # Save the learned weights
-    adapt.save_weights(test_name='does_this_work')
+    adapt.save_weights(test_name=test_name)
+    # get save location of weights to save tracked data in same directory
+    [location, run_num] = adapt.weights_location(test_name=test_name)
 
-    ee_track = np.array(ee_track)
-    target_track = np.array(target_track)
+    print('Average loop speed: ', sum(time_track)/len(time_track))
+    print('Run number ', run_num)
+    print('Saving tracked data to ', location)
 
-    if ee_track.shape[0] > 0:
-        # plot distance from target and 3D trajectory
-        import matplotlib
-        matplotlib.use("TKAgg")
-        import matplotlib.pyplot as plt
-        from abr_control.utils.plotting import plot_3D
+    np.savez_compressed(location + '/q%i' % (run_num), q=[q_track])
+    np.savez_compressed(location + '/time%i' % (run_num), time=[time_track])
+    np.savez_compressed(location + '/u%i' % (run_num), q=[u_track])
+    np.savez_compressed(location + '/adapt%i' % (run_num), q=[adapt_track])
 
-        plt.figure()
-        plt.plot(np.sqrt(np.sum((np.array(target_track) -
-                                 np.array(ee_track))**2, axis=1)))
-        plt.ylabel('Distance (m)')
-        plt.xlabel('Time (ms)')
-        plt.title('Distance to target')
-
-        plot_3D(ee_track, target_track)
-        plt.show()
+    # ee_track = np.array(ee_track)
+    # target_track = np.array(target_track)
+    #
+    # if ee_track.shape[0] > 0:
+    #     # plot distance from target and 3D trajectory
+    #     import matplotlib
+    #     matplotlib.use("TKAgg")
+    #     import matplotlib.pyplot as plt
+    #     from abr_control.utils.plotting import plot_3D
+    #
+    #     plt.figure()
+    #     plt.plot(np.sqrt(np.sum((np.array(target_track) -
+    #                              np.array(ee_track))**2, axis=1)))
+    #     plt.ylabel('Distance (m)')
+    #     plt.xlabel('Time (ms)')
+    #     plt.title('Distance to target')
+    #
+    #     plot_3D(ee_track, target_track)
+    #     plt.show()
