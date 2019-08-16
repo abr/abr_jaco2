68d87230da39241be9698db76da6b5912e46f452
p3jawors <p3jawors@gmail.com>
2019-08-16 13:21:58 -0400

    Added python version check to install
    - added python requires to limit to python<=3.6.9 due to issues
      with cython and python 3.7
    - added exception to throw error on import if python>=3.7

--- abr_jaco2/__init__.py
+++ abr_jaco2/__init__.py
@@ -1,3 +1,14 @@
 from .config.config import Config
 from .interface.interface import Interface
 from .interface import jaco2_rs485
+import sys
+
+if sys.version_info > (3, 6, 9):
+    raise ImportError(
+        """
+You are using Python version %s and abr_jaco2
+currently supports python up to 3.6.9.
+
+Please create a new environment with python =<3.6.9
+"""
+% (sys.version))
